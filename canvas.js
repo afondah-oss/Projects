c68ee1a8c71879cd3d95b8783680c9ab306fc6c4
afondah-oss <afondah@gmail.com>
2025-09-16 15:13:04 +0200

    Can you see this

---
+++ canvas.js
@@ -0,0 +1,42 @@
+//var canvas=document.querySelector('canvas');
+
+//canvas.width=window.innerWidth;
+//canvas.height=window.innerHeight;
+
+
+//var c=canvas.getContext('2d');
+
+//c.fillStyle="rgba(255,0,0,0.5)";
+//c.fillRect (100,100,100,100);
+//c.fillStyle="rgba(2, 0, 0, 0.9)";
+//c.fillRect (300,100,100,100);
+//c.fillStyle="rgba(0, 60, 255, 0.5)";
+//c.fillRect (400,400,100,100);
+
+
+//Line
+//c.beginPath();
+//c.moveTo(50,300);
+//c.lineTo(300,100);
+//c.lineTo(300,400);
+//c.lineTo(500,500);
+//c.strokeStyle="blue";
+//c.stroke();
+
+//Arc /Circle 
+//c.beginPath();
+//c.arc(300, 300,30,0 ,Math.PI*2,false);
+//c.strokeStyle ='red';
+//c.stroke();
+
+//for (var i =0; i<180; i++) {
+   // var x = Math.random() * window.innerWidth;
+   // var y = Math.random() * window.innerHeight;
+   // c.beginPath();
+   // c.arc(x,y,30,0 ,Math.PI*2,false);
+   // c.strokeStyle ='red';
+  // c.strokeStyle='green';
+//c.stroke();
+//}
+//console.log(canvas);
+
